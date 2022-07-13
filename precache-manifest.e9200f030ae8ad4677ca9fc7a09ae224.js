912458be86a6e3b6aca11d63de555d98566e6f7b
radna0 <87762857+radna0@users.noreply.github.com>
2022-07-13 17:02:29 +0700

    Updates

---
+++ precache-manifest.e9200f030ae8ad4677ca9fc7a09ae224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ac52a1ff09ba789f0ee",
+    "url": "/estore/static/css/main.538e3ae2.chunk.css"
+  },
+  {
+    "revision": "4ac52a1ff09ba789f0ee",
+    "url": "/estore/static/js/main.4ac52a1f.chunk.js"
+  },
+  {
+    "revision": "37986676adb42383fc21",
+    "url": "/estore/static/js/1.37986676.chunk.js"
+  },
+  {
+    "revision": "cc2282924b4e4bda3b35",
+    "url": "/estore/static/js/runtime~main.cc228292.js"
+  },
+  {
+    "revision": "1af0ab695131728c77968efb8197315a",
+    "url": "/estore/index.html"
+  }
+];
